312dafee2e0b59c099dc729c8a0d80b70ee1a461
unknown <jink9797@naver.com>
2023-06-20 11:22:02 +0900

    css_modify

--- src/components/ai_start/introduce.js
+++ src/components/ai_start/introduce.js
@@ -15,7 +15,7 @@ function Introduce() {
       <div className="default-container">
         <div className="introduce-container">
           <div className='ex'>
-            <p><strong>*선택(권장)</strong></p>
+            <p><strong style={{color: 'blue'}}>*선택(권장)</strong></p>
             <h3>자기소개서를 입력해 주세요</h3>
             <h4>(협업 경험, 대회 및 공모전, 개인 경험 등)</h4>
             <p>자기소개서를 입력하시면 추천정확도가 올라갑니다</p>
