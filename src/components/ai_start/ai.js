8bd0569a9e5dc95a516e68347babcde83ee3b8f6
Atopiano <121912367+Atopiano@users.noreply.github.com>
2023-06-24 16:35:35 +0900

    로렘입숨 https api test

--- src/components/ai_start/ai.js
+++ src/components/ai_start/ai.js
@@ -1,35 +1,28 @@
-import React, { useState } from 'react';
-import Spinner from './spinner.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const Ai = () => {
-  const [isLoading, setLoading] = useState(false);
+function Ai() {
+  const [randomImageUrl, setRandomImageUrl] = useState('');
 
-  const handleClick = () => {
-    setLoading(true);
+  useEffect(() => {
+    const getRandomImage = async () => {
+      try {
+        const response = await axios.get('https://picsum.photos/200/300'); // 이미지 크기 조정 가능
+        const imageUrl = response.request.responseURL;
+        setRandomImageUrl(imageUrl);
+      } catch (error) {
+        console.error('Error fetching random image:', error);
+      }
+    };
 
-    setTimeout(() => {
-      // 최소 3초 후에 다음 페이지로 넘어감
-      window.location.href = '다음 페이지 URL';
-    }, 3000);
-  };
+    getRandomImage();
+  }, []);
 
   return (
     <div>
-      <div className={isLoading ? 'blur' : ''}>
-        <h1>결과 보기</h1>
-        <button onClick={handleClick}>결과 보기</button>
-      </div>
-      {isLoading && (
-        <div className="loading-container">
-          <div className="loading-overlay"></div>
-          <div className="loading-content">
-            <p>로딩 중입니다.</p>
-            <img src={Spinner} alt="로딩중" />
-          </div>
-        </div>
-      )}
+      {randomImageUrl && <img src={randomImageUrl} alt="Random Image" />}
     </div>
   );
-};
+}
 
 export default Ai;
