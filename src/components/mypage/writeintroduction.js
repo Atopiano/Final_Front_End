62fe93f0bb5a976d6b5c955050c1484dca855d81
Atopiano <121912367+Atopiano@users.noreply.github.com>
2023-06-26 05:13:23 +0900

    자기소개서 작성 폼 구현

--- src/components/mypage/writeintroduction.js
+++ src/components/mypage/writeintroduction.js
@@ -2,17 +2,20 @@ import React from 'react';
 import Header from '../base/header';
 import Footer from '../base/footer';
 import MySidebar from './mysidebar';
-import '../../components/style/mystack.css';
-
+import '../../components/style/writeintroduction.css';
 
 function WriteIntroduction() {
   return (
     <>
       <Header />
-      <div className="default-container">
-        <MySidebar />
-        <div className="content">
-          <h1>자기소개서 작성하기</h1>
+      <div className='default-container'>
+      <MySidebar />
+        <div className="custom-container">
+          <div className="custom-sidebar">
+          </div>
+          <div className="custom-content">
+            <h1 className="custom-title">자기소개서 작성하기</h1>
+          </div>
         </div>
       </div>
       <Footer />
